782ee5554dc22b8df795c23529fc2736374d4aef
Zachariah <spencerzachariah@gmail.com>
2025-06-07 13:35:32 -0500

    add qol ui/ux stuff to lab encounter

--- app/scenes/alchemy_lab.rb
+++ app/scenes/alchemy_lab.rb
@@ -53,22 +53,81 @@ class AlchemyLab
 
     if $encounter_manager.encounters_completed? == 0 and $tutorials
       puts "RUN TUTORIAL"
-      @tutorials = InfoBoxChain.new([
-        [
-          { x:  GTK.args.grid.w / 2 - 300, y: 500, width:600, height:80, text:"Within the laboratory you can", duration:180 },
-          { x: GTK.args.grid.w / 2 - 300, y: 420, width:600, height:80, text:"combine ingredients with a",   duration:180 },
-          { x: GTK.args.grid.w / 2 - 300, y: 340, width:600, height:80, text:"bottle to craft powerful potions.",   duration:180 },
-        ],
-        [
-          { x:  GTK.args.grid.w / 2 - 300, y: 500, width:600, height:80, text:"You can also pick up to 5", duration:180 },
-          { x: GTK.args.grid.w / 2 - 300, y: 420, width:600, height:80, text:"ingredients to take with you.",   duration:180 },
-        ],
-        [
-          { x:  GTK.args.grid.w / 2 - 300, y: 500, width:600, height:80, text:"Craft up to 10 potions for your", duration:180 },
-          { x: GTK.args.grid.w / 2 - 300, y: 420, width:600, height:80, text:"deck before you start your",   duration:180 },
-          { x: GTK.args.grid.w / 2 - 300, y: 340, width:600, height:80, text:"journey into the wilderness.",   duration:180 },
-        ],
-      ])
+      @tutorials =
+        InfoBoxChain.new(
+          [
+            [
+              {
+                x: GTK.args.grid.w / 2 - 300,
+                y: 500,
+                width: 600,
+                height: 80,
+                text: "Within the laboratory you can",
+                duration: 180
+              },
+              {
+                x: GTK.args.grid.w / 2 - 300,
+                y: 420,
+                width: 600,
+                height: 80,
+                text: "combine ingredients with a",
+                duration: 180
+              },
+              {
+                x: GTK.args.grid.w / 2 - 300,
+                y: 340,
+                width: 600,
+                height: 80,
+                text: "bottle to craft powerful potions.",
+                duration: 180
+              }
+            ],
+            [
+              {
+                x: GTK.args.grid.w / 2 - 300,
+                y: 500,
+                width: 600,
+                height: 80,
+                text: "You can also pick up to 5",
+                duration: 180
+              },
+              {
+                x: GTK.args.grid.w / 2 - 300,
+                y: 420,
+                width: 600,
+                height: 80,
+                text: "ingredients to take with you.",
+                duration: 180
+              }
+            ],
+            [
+              {
+                x: GTK.args.grid.w / 2 - 300,
+                y: 500,
+                width: 600,
+                height: 80,
+                text: "Craft up to 10 potions for your",
+                duration: 180
+              },
+              {
+                x: GTK.args.grid.w / 2 - 300,
+                y: 420,
+                width: 600,
+                height: 80,
+                text: "deck before you start your",
+                duration: 180
+              },
+              {
+                x: GTK.args.grid.w / 2 - 300,
+                y: 340,
+                width: 600,
+                height: 80,
+                text: "journey into the wilderness.",
+                duration: 180
+              }
+            ]
+          ]
+        )
     end
   end
 
@@ -136,7 +195,6 @@ class AlchemyLab
 
     # overwrite the player's ingredients with this collection
     @player.ingredients = Inventory.new(new_cards)
-
 
     $encounter_manager.inc_encounters_completed
     $game.change_scene(prev_sc: @sc_id, next_sc: "map")
@@ -254,11 +312,56 @@ class AlchemyLab
       l3 << [front_card, sel_cards]
       l3
     when 4
+      if @craftable_potion
+        puts @craftable_potion
+        craftable_potion_label ||= {
+          x: GTK.args.grid.w / 2,
+          y: GTK.args.grid.h / 2 + 150,
+          text: "#{@craftable_potion.data.name}",
+          anchor_x: 0.5,
+          anchor_y: 0.5,
+          r: 255,
+          g: 0,
+          b: 0,
+          size_enum: 15,
+          primitive_marker: :label
+        }
+
+        l4 << [craftable_potion_label, craft_btn]
+      end
+
+      uses_left_label ||= {
+        x: GTK.args.grid.w / 2,
+        y: 200,
+        alignment_enum: 1,
+        size_enum: 8,
+        r: 255,
+        g: 255,
+        b: 255,
+        text: "Brewing Capacity: #{@uses_left}",
+        primitive_marker: :label
+      }
+
+      ingredients_stored_label ||= {
+        x: 100,
+        y: 75,
+        alignment_enum: 1,
+        size_enum: 8,
+        r: 255,
+        g: 255,
+        b: 255,
+        text: "#{@ing_menu_widget.items?.size} / #{@max_ingredients}",
+        primitive_marker: :label
+      }
+
       l4 << [
         @ing_menu_widget.render,
         @pot_menu_widget.render,
-        @stored_ing_menu_widget.render
+        @stored_ing_menu_widget.render,
+        uses_left_label,
+        ingredients_stored_label
       ]
+
       l4
     else
       # puts "combat.rb: Invalid Render Argument"
@@ -367,6 +470,58 @@ class AlchemyLab
     }
   end
 
+  def craft_btn
+    GTK.args.outputs[:craft_btn].w = 150
+    GTK.args.outputs[:craft_btn].h = 75
+
+    GTK.args.outputs[:craft_btn].primitives << {
+      x: 0,
+      y: 0,
+      w: 150,
+      h: 75,
+      angle: 0,
+      r: 0,
+      g: 0,
+      b: 0,
+      primitive_marker: :solid
+    }
+
+    GTK.args.outputs[:craft_btn].primitives << {
+      x: 5,
+      y: 5,
+      w: 140,
+      h: 65,
+      angle: 0,
+      r: 70,
+      g: 70,
+      b: 150,
+      a: 100,
+      primitive_marker: :solid
+    }
+
+    GTK.args.outputs[:craft_btn].primitives << {
+      x: 150 / 2,
+      y: 75 / 2,
+      text: "CRAFT",
+      anchor_x: 0.5,
+      anchor_y: 0.5,
+      r: 255,
+      g: 255,
+      b: 255,
+      size_enum: 3
+    }
+
+    {
+      x: GTK.args.grid.w / 2 - 75,
+      y: GTK.args.grid.h - 75 - 100,
+      w: 150,
+      h: 75,
+      angle: 0,
+      path: :craft_btn,
+      primitive_marker: :sprite
+    }
+  end
+
   def leave_btn
     GTK.args.outputs[:leave_btn].w = 150
     GTK.args.outputs[:leave_btn].h = 75
@@ -446,6 +601,15 @@ class AlchemyLab
          Geometry.intersect_rect?(inputs.mouse, leave_btn)
       puts "clicked on leave_btn"
       leave
+    end
+
+    if GTK.args.inputs.mouse.click &&
+        Geometry.intersect_rect?(inputs.mouse, craft_btn) &&
+        @craftable_potion
+
+      puts "clicked on craft_btn"
+      craft(@craftable_potion.id)
+      @craftable_potion = nil
     end
 
     if clicked = @pot_menu_widget.selected_item
