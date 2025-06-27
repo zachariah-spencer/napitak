69c68178d3bdea17305c9c91a07e6ee3d1679907
Zachariah <spencerzachariah@gmail.com>
2025-06-26 21:53:23 -0500

    fix a render bug with created_at on status_labels

--- app/game.rb
+++ app/game.rb
@@ -112,7 +112,9 @@ class Game
     handle_pause
 
     if @new_status_label_queued && @status_label_queue_count.elapsed_time >= 0.75.seconds
-      @status_labels << @status_labels_queue.shift
+      new_label = @status_labels_queue.shift
+      new_label.created_at = Kernel.tick_count
+      @status_labels << new_label
       @new_status_label_queued = false
     end
 
