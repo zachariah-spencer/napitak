04111a805d053da0e116cac6e315d1c87a81b1da
Zachariah <spencerzachariah@gmail.com>
2025-07-03 23:41:56 -0500

    Play with tutorial stuff

--- app/models/enemies/abyssal_tutorial.rb
+++ app/models/enemies/abyssal_tutorial.rb
@@ -15,7 +15,7 @@ class AbyssalTutorial < Enemy
     $enemy = self
     @combat_stats =
       CombatStatsComponent.new(
-        hp: 30,
+        hp: 50,
         focus: 0,
         x: GTK.args.grid.w / 2,
         y: GTK.args.grid.h - 270
