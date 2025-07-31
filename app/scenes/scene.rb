c026e1c509e83fc93c70debaed1ccbc88fdafcee
Zachariah <spencerzachariah@gmail.com>
2025-07-31 16:29:15 -0500

    build out more combat ui, fix a bug with collections when cards are in hand

--- app/scenes/scene.rb
+++ app/scenes/scene.rb
@@ -1,11 +1,16 @@
 class Scene
+  attr :sc_id
   attr_gtk
 
-  def tick; end
+  def tick
+  end
 
-  def render(_layer_num); end
+  def render(_layer_num)
+  end
 
-  def ready; end
+  def ready
+  end
 
-  def cleanup; end
+  def cleanup
+  end
 end
